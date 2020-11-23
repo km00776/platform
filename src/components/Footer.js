b665c5346e04c6c7014074ee659f6e47df89453a
km00776 <kmurugathas@gmail.com>
2020-11-23 11:28:04 +0000

    adjustments in platform screen

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import styles from '../styles/Footer.module.scss';
 const Footer = () => {
     return(
     <div className={styles.container}>
-       <p> © 2014 – 2020 English Language Testing Ltd </p>
+       <p className={styles.license}> © 2014 – 2020 English Language Testing Ltd </p>
       </div>
       
     );
