da9976b9f05ab6d2a2cb273e3996d961ca858b85
km00776 <kmurugathas@gmail.com>
2020-12-05 18:11:33 -0800

    positioning

--- src/components/ClientSetupScreen.js
+++ src/components/ClientSetupScreen.js
@@ -26,42 +26,45 @@ const ClientSetup = () => {
                             <option value="University">University</option>
                         </select>
                     </div>
-                        <div className={styles.calender}>
-                            <p className={styles.dateCreated}>Date Created</p>
-                                <input className={styles.dateFrom}></input>
-                                <input className={styles.dateTo}></input>
-                        </div>
                         <div className={styles.ArchivedContainer}>
-                            <p className={styles.Archived}>Is Archived?</p>
-                                {/* <div className={styles.ArchivedCheckbox}>
+                            <p className={styles.Archived}>Active</p>
+                                <div className={styles.ArchivedCheckbox}>
                                     <label className={styles.space} for="box1">All</label>
                                     <input className={styles.space} type="radio" id="box1" name="box1" value="All"></input>
                                     <label className={styles.space} for="box1">Yes</label>
                                     <input className={styles.space} type="radio" id="box1" name="box1" value="Yes"></input>
-                                    <label className={styles.space} for="box1">No</label>
-                                    <input className={styles.space} type="radio" id="box1" name="box1" value="No"></input>F
-                                </div> */}
+                                      <label className={styles.space} for="box1">No</label>
+                                    <input className={styles.space} type="radio" id="box1" name="box1" value="No"></input>
+                                </div>
                         </div>
                 </div>
-                {/* <div className={styles.rightside}>
+                <div className={styles.rightside}>
                     <div className={styles.name}>
-                        <label className={styles.nameLabel} for="name">Find</label>
+                        <label className={styles.nameLabel} for="name">Name</label>
                         <input className={styles.nameInput} id="name" name="name"></input>
                     </div>
                     <div className={styles.Country}>
                         <label className={styles.countryLabel} for="name">Country</label>
                         <input className={styles.countryInput} id="name" name="name"></input>
                     </div>
-                    <div className={styles.ActiveCheckbox}>
-                        <label className={styles.countryLabel} for="name">Active</label>
-                        <label className={styles.space} for="box1">All</label>
-                        <input className={styles.space} type="radio" id="box1" name="box1" value="All"></input>
-                        <label className={styles.space} for="box1">Yes</label>
-                        <input className={styles.space} type="radio" id="box1" name="box1" value="Yes"></input>
-                        <label className={styles.space} for="box1">No</label>
-                        <input className={styles.space} type="radio" id="box1" name="box1" value="No"></input>
+                    <div className={styles.calender}>
+                            <p className={styles.dateCreated}>Date Created</p>
+                                <input className={styles.dateFrom}></input>
+                                <input className={styles.dateTo}></input>
                     </div>
-                </div> */}
+                    <div className={styles.ArchivedContainer}>
+                            <p className={styles.Archived}>Is Archived</p>
+                                <div className={styles.ArchivedCheckbox}>
+                                    <label className={styles.space} for="box1">All</label>
+                                    <input className={styles.space} type="radio" id="box1" name="box1" value="All"></input>
+                                    <label className={styles.space} for="box1">Yes</label>
+                                    <input className={styles.space} type="radio" id="box1" name="box1" value="Yes"></input>
+                                      <label className={styles.space} for="box1">No</label>
+                                    <input className={styles.space} type="radio" id="box1" name="box1" value="No"></input>
+                                </div>
+                        </div>
+                    
+                </div>
             </div>
             <Footer />
         </div>
