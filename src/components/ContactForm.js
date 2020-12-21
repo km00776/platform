3b16d5a2e9d21af4d344cae2de6c291f04860264
km00776 <kmurugathas@gmail.com>
2020-12-20 17:25:14 -0800

    changes to form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -25,33 +25,35 @@ return (
        <h2 className={styles.contact}><i onClick={props.onClick}class="chevron circle down icon"></i>Contact</h2> 
     </div>
     <div>
-        <div>
+        <i class="envelope icon"></i>
         <div className={styles.formInput}>
-                    {/* <div className={styles.name}>
-                        <label className={styles.Label}><i class="info circle icon"></i>Your Name:</label>
+                    <div className={styles.name}>
+                        <label className={styles.Label}>Your Name:</label>
                         <br></br>
                         <input className={styles.Input}></input>
                     </div>
                     <br></br>
                     <div className={styles.email}>
-                        <label className={styles.Label}><i class="info circle icon"></i>E-mail:</label>
+                        <label className={styles.Label}>E-mail:</label>
                         <br></br>
                         <input type="password" className={styles.Input}></input>
                     </div>
                     <div className={styles.subject}>
-                        <label className={styles.Label}><i class="info circle icon"></i>Subject:</label>
+                        <label className={styles.Label}>Subject:</label>
                         <br></br>
                         <input type="password" className={styles.Input}></input>
                     </div>
                     <div className={styles.message}>
-                        <label className={styles.Label}><i class="info circle icon"></i>Message:</label>
+                        <label className={styles.Label}>Message:</label>
                         <br></br>
                         <input type="password" className={styles.Input}></input>
-                    </div> */}
+                    </div>
+                    
+                <button class="ui inverted primary button">Submit</button>
                 
         </div>
             
-        </div>
+        
     </div>
 </div>)
 
