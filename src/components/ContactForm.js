d852fc7e14259a99a0f45f5812744bfa355cf102
km00776 <kmurugathas@gmail.com>
2020-12-25 16:03:55 +0000

    MaterialUI field added

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,10 @@
 
 
 import styles from '../styles/ContactForm.module.scss';
+import {BasicTextFields} from './UI/SupportForm.js';
+
+
+
 
 const ContactForm = (props) => {
 
@@ -24,26 +28,8 @@ const SupportForm = (props) => {
             <div className={styles.headingFormContainer}>
                 <h2 className={styles.contact}><i onClick={props.onClick} class="chevron circle down icon"></i>Contact</h2>
             </div>
-            <div className={styles.contactName}>
-                <div className={styles.name}>
-                    <label>Name:</label>
-                    <input className={styles.nameInput}></input>
-                </div>
-                 <div className={styles.institutionName}>
-                    <label>Client:</label>
-                    <input className={styles.institutionInput}></input>
-                </div>
-                 <div className={styles.emailForm}>
-                     <label>Email:</label>
-                     <input className={styles.emailInput}></input>
-                 </div>
-                 <div className={styles.testCode}>
-                    <label>Test Code:</label>
-                    <input className={styles.testCodeInput}></input>
-                </div>
-                <div className={styles.submitButton}>
-                    <button class="positive ui button">Submit Query</button>
-                </div> 
+            <div className={styles.contactDetails}>
+                <BasicTextFields />
             </div>
         </div>)
 
