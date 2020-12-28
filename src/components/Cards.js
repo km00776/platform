7139d1c94a621dff3bbeec17d7e80074c5cf186a
km00776 <kmurugathas@gmail.com>
2020-12-28 14:32:15 +0000

    made changes to the page@

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -82,19 +82,19 @@ export function SimplePaper() {
     <div className={classes.root}>
       <div className={styles.t3}> 
       <Paper style={{ backgroundColor: 'white', height: '140px' }} elevation={8}> 
-        <TrendingUpIcon style={{ fontSize: 118, color: 'lightgreen' }} />
+        <TrendingUpIcon style={{ fontSize: 118, color: '#00FF00	' }} />
         {/* <h2 className={styles.papertext}>Total Clients</h2> */}
          </Paper>
       </div>
       <div  className={styles.t2}> 
       <Paper style={{ backgroundColor: 'white', height: '140px' }} elevation={8}>
-        <ImportContactsIcon style={{ fontSize: 100, color: 'lightskyblue' }} />
+        <ImportContactsIcon style={{ fontSize: 100, color: '#FF1493	' }} />
         {/* <h2 className={styles.papertext2}>Total University Clients</h2> */}
       </Paper>
       </div>
       <div  className={styles.t1}>
       <Paper style={{ backgroundColor: 'white', height: '140px' }} elevation={8}>
-        <SchoolIcon style={{ fontSize: 100, color: '#fed8b1' }} />
+        <SchoolIcon style={{ fontSize: 100, color: '#FF4500' }} />
         {/* <h2 className={styles.papertext3}>Total School Clients</h2> */}
       </Paper>
       </div> 
