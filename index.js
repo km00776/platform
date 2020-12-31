ffb204070b6e47878aaa710eea5602b9de1e91e8
km00776 <kmurugathas@gmail.com>
2020-12-31 18:17:08 +0000

    port changed to PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const platformdb = require('./platformdb.js');
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 const path = require("path");
 app.use(cors());
 app.use(express.json());
