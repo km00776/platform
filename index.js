404c186ec9ef603c401d91cd22f6917ef0f9ffa7
km00776 <kmurugathas@gmail.com>
2020-12-31 20:12:49 +0000

    changes to index.js for server

--- index.js
+++ index.js
@@ -9,18 +9,24 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
     
-    app.use(express.static(path.join(__dirname, "Client/build")));
+    app.use(express.static("Client/build"));
+
+    
+    app.get('*', (req, res) => {
+      res.sendFile(path.resolve(__dirname, 'Client', 'build', 'index.html'));
+    });
+  
   }
 
 console.log(__dirname);
 console.log(path.join(__dirname, "Client/build"));
 
-// app.use(function (req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-//     next();
-//   });
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+    next();
+  });
 
 app.get('/clients', (req, res) => {
     platformdb.getClients().then(response => {
