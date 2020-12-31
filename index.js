4701058f06fc39380a54ef4db1ecb5eba6a4d32a
km00776 <kmurugathas@gmail.com>
2020-12-31 20:37:12 +0000

    commented

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ if (process.env.NODE_ENV === "production") {
 console.log(__dirname);
 console.log(path.join(__dirname, "Client/build"));
 
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
-    next();
-  });
+// app.use(function (req, res, next) {
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//     res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
+//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Access-Control-Allow-Headers');
+//     next();
+//   });
 
 app.get('/clients', (req, res) => {
     platformdb.getClients().then(response => {
