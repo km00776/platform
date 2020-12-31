100e3687fee9faf85afac95ecd978a240480e81a
km00776 <kmurugathas@gmail.com>
2020-12-31 18:03:20 +0000

    made xchanges

--- index.js
+++ index.js
@@ -8,8 +8,7 @@ app.use(cors());
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    //server static content
-    //npm run build
+    
     app.use(express.static(path.join(__dirname, "client/build")));
   }
 
