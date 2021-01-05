10ae18d689765b80aec1b84ad68d624731581604
km00776 <kmurugathas@gmail.com>
2021-01-05 21:42:19 +0000

    back to clientsetup

--- Client/src/App.js
+++ Client/src/App.js
@@ -12,7 +12,7 @@ export default class App extends Component {
         render() {
             return(  
             // should be now
-              <ClientDetails    />
+              <ClientSetup    />
               
 
             )
