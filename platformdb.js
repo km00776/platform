2b875003a5f65ffecccfe270f9a71a762aae3590
km00776 <kmurugathas@gmail.com>
2021-01-12 22:03:05 +0000

    added date for postgresql@

--- platformdb.js
+++ platformdb.js
@@ -9,7 +9,7 @@ const proConfig = process.env.DATABASE_URL; //heroku addons
 const pool = new Pool({
     connectionString:
       process.env.NODE_ENV === "production" ? proConfig : devConfig,
-      // ssl: { rejectUnauthorized: false }
+      ssl: { rejectUnauthorized: false }
   });
 
 // const pool = new Pool({
