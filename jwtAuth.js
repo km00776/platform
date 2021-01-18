862619d285d906cf0c97052c68afac3ec9ed2ea5
km00776 <kmurugathas@gmail.com>
2021-01-17 19:48:39 -0800

    need to add middleware

---
+++ jwtAuth.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
+const pool = require('./platformdb.js');
+const jwtGenerator = require('./jwtGenerator');
+
+
+// login   route;
+
+router.post("/login", async(req,res) => {
+    try {
+        // 1. destrutcure req.body
+
+        const {login, password} = req.body;
+
+        const users = await pool.query("SELECT * FROM users WHERE login = $1", [login])
+
+        if(users.rows.length === 0) {
+            return res.status(401).json("Login or Password Invalid");
+        }
+
+             // if(validPassowrd)
+        // 2. does user exist, if not throw error
+        const validPassowrd = await bcrypt.compare(password, user.rows[0].password);
+
+        if(!validPassowrd) {
+            return res.status(401).json("Password or Login Invalid");
+        }
+
+        const token = jwtGenerator(user.rows[0].login);
+
+        res.json({token});
+
+        // if(validPassowrd)
+        // 2. does user exist, if not throw error
+
+ 
+    }
+    catch(error) {
+        console.log(error.message);
+        res.status(500).send("Server Flopped");
+    }
+})
